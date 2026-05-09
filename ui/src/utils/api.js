064dd6ae33e56231f8b615708ba454d583826fa5
Bikash Panda <oksbwn@gmail.com>
2026-05-09 18:10:05 +0530

    fix(api): add auth interceptor and increase request timeouts

--- ui/src/utils/api.js
+++ ui/src/utils/api.js
@@ -2,10 +2,21 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: '/api/v1',
-  timeout: 10000,
+  timeout: 60000, // Increased for long-running scans
   headers: {
     'Content-Type': 'application/json'
   }
+})
+
+// Add Auth Interceptor
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
 })
 
 export default api
